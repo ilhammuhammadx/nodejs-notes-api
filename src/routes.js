c7d6d92bb96e4fc1db9a2023099afed0ca46b97b
Ilham Muhammad <ilhamhd98@gmail.com>
2021-05-11 22:55:24 +0700

    update

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,10 @@
-const { addNoteHandler, getAllNotesHandler } = require('./handler');
+const { 
+    addNoteHandler, 
+    getAllNotesHandler, 
+    getNoteByIdHandler,
+    editNoteByIdHandler,
+    deleteNoteByIdHandler
+} = require('./handler');
 
 const routes = [
     {
@@ -14,8 +20,18 @@ const routes = [
     {
         method: 'GET',
         path: '/notes/{id}',
-        handler: () => {},
-      },
+        handler: getNoteByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+    },
 ]
 
 module.exports = routes
